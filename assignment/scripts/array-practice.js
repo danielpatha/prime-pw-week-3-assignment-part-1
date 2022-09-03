7118f097a9a5dae90e223a9fc11da385f93a0896
Daniel Pathammavong <patha037@umn.edu>
2022-09-03 13:09:42 -0500

    Accessed items in array and adding/removing items

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -32,14 +32,13 @@ console.log('--- 3. Accessing items in an array ---');
 console.log('First animal is', animalArray[0])
 
 // 3.a. TODO: Log the second animal in the array 
-
+console.log('Second animal is', animalArray[1])
 
 // 3.b. TODO: Log the last animal in the array using it's array index 
-
+console.log('The last animal is', animalArray[3])
 
 // 3.c. (STRETCH) TODO: Log the last animal by using the array length, 
 //      instead of the exact index number of the last item
-
 
 
 // 4. Adding & Removing Array Items
@@ -50,7 +49,8 @@ animalArray.push('penguin');
 console.log('Added an animal to end,', animalArray);
 
 // 4.a. TODO: Add a new food at the end of your array & log the array
-
+favoriteFoods.push('cookies');
+console.log('Added another food item to end',favoriteFoods)
 
 
 // Example: Remove the last animal by using Array.pop
@@ -60,6 +60,9 @@ console.log('The animals are now', animalArray);
 
 // 4.b. TODO: Remove the food at the end of your array & 
 //      log both the food removed and the updated array
+let removedFood = favoriteFoods.pop();
+console.log('Removed the last food item', removedFood);
+console.log('The foods are now', favoriteFoods);
 
 
 // Example: Add an animal to the beginning using Array.unshift
